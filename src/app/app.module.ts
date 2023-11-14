1e96324a5866116a55252ef6f8e79eb3415285ba
Hugomes99 <hugochristian99@gmail.com>
2023-11-13 22:04:44 -0300

    Função da data funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FunciAddEditComponent } from './funci-add-edit/funci-add-edit.component';
+
 
 
 
@@ -45,7 +47,8 @@ import { FunciAddEditComponent } from './funci-add-edit/funci-add-edit.component
     HttpClientModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
